732171e340c5bef7fcb32dc7c168b51b5dab685f
nicolas <nicolas.brietpro@gmail.com>
2026-01-09 18:55:52 +0100

    fix(testbench): update callback responses to use consistent key format

--- modules/testbench/client/main.lua
+++ modules/testbench/client/main.lua
@@ -87,13 +87,13 @@ end)
 -- Fermer l'interface
 RegisterNUICallback('testbench:close', function(data, cb)
     TriggerEvent('testbench:close')
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Scanner les modules
 RegisterNUICallback('testbench:scanModules', function(data, cb)
     TriggerServerEvent('testbench:scanModules')
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Obtenir les données initiales
@@ -104,39 +104,39 @@ RegisterNUICallback('testbench:getInitialData', function(data, cb)
     -- Le serveur répondra via un event qui mettra à jour l'UI
     -- Retourner une structure valide en attendant
     cb({
-        modules = {},
-        scenarios = {}
+        ['modules'] = {},
+        ['scenarios'] = {}
     })
 end)
 
 -- Obtenir les détails d'un module
 RegisterNUICallback('testbench:getModuleDetails', function(data, cb)
     TriggerServerEvent('testbench:getModuleDetails', data.moduleName)
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Exécuter tous les tests
 RegisterNUICallback('testbench:runAllTests', function(data, cb)
     TriggerServerEvent('testbench:runAllTests')
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Exécuter un test spécifique
 RegisterNUICallback('testbench:runTest', function(data, cb)
     TriggerServerEvent('testbench:runTest', data.testName)
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Arrêter les tests
 RegisterNUICallback('testbench:stopTests', function(data, cb)
     TriggerServerEvent('testbench:stopTests')
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Exécuter un scénario
 RegisterNUICallback('testbench:runScenario', function(data, cb)
     TriggerServerEvent('testbench:runScenario', data.scenario)
-    cb({ success = true })
+    cb({ ['success'] = true })
 end)
 
 -- Exporter les résultats
